4d929ed6e8900a5abb22c4236020b0ba8a798f6e
korren <m.woelke@hotmail.de>
2021-12-24 17:54:24 +0100

    update install script

--- first-install.sh
+++ first-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer install" 
 docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:schema:create --no-interaction
 docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:fixtures:load --no-interaction
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application yarn encore dev"
