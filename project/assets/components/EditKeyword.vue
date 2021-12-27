427fd25d830e000a4d12509ff52c43046f5ddec5
korren <m.woelke@hotmail.de>
2021-12-27 01:02:22 +0100

    begin edit keyword

--- project/assets/components/EditKeyword.vue
+++ project/assets/components/EditKeyword.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-      8=============D
+      <div class="row">
+          <h1 class="col-12">{{store.data.state.activeDomain.name}}</h1>
+      </div>
+      <hr>
+      <div class="row">
+          <h2 class="col-12">Edit keyword groups</h2>
+      </div>
+      <div class="row">
+          <button class="btn btn-outline-secondary"  v-for="keywordGroup in keywordGroups" :key="keywordGroup['@id']">{{keywordGroup.name}}</button>
+      </div>
+
   </div>
 </template>
 
@@ -12,11 +22,15 @@ export default {
   data() {
     return {
       keyword: null,
+      keywordGroups: null,
     };
   },
   async mounted() {
     this.keyword = await api.apiGetKeyword(this.$route.params.keyword);
-    api.apiLockKeyword(this.keyword.id);
-  }
+    this.keywordGroups = await api.apiGetKeywordGroupsForDomain(
+      this.store.data.state.activeDomain.id
+    );
+    console.log(api.apiLockKeyword(this.keyword.id));
+  },
 };
 </script>
