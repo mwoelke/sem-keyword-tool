459e759fa58bc61b0f6c191639514544df1bdab3
korren <m.woelke@hotmail.de>
2022-01-02 14:13:00 +0100

    fix fixtures not loading

--- install.sh
+++ install.sh
@@ -7,10 +7,9 @@ while getopts "f" arg; do
             ;;
     esac
 done
-#docker exec sem-keyword_php-apache bash -c "cd /app && gosu application php bin/console doctrine:schema:drop --force --no-interaction" 
 docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer install" 
-#docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:schema:create --no-interaction
 if  [ "$fixtures" == 1 ]; then
+    docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer require doctrine/doctrine-fixtures-bundle"
     docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:fixtures:load --no-interaction
 fi
 docker exec sem-keyword_php-apache bash -c "cd /app && gosu application yarn install "
