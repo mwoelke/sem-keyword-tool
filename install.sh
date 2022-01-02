05045b172533de17d4674e603f387bb82cf0f363
korren <m.woelke@hotmail.de>
2022-01-02 17:45:20 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ while getopts "f" arg; do
     esac
 done
 docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer install" 
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application php bin/console doctrine:database:drop --force --no-interaction" 
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application php bin/console doctrine:database:create --no-interaction" 
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application php bin/console doctrine:schema:create --no-interaction" 
 if  [ "$fixtures" == 1 ]; then
     docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer require doctrine/doctrine-fixtures-bundle"
     docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:fixtures:load --no-interaction
