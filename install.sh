07ce945cad6551b88fcb22cc557a0304ea4c269a
korren <m.woelke@hotmail.de>
2022-01-02 12:20:06 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer install" 
+fixtures=1
+while getopts "f" arg; do
+    case $arg in 
+        f)
+            fixtures=0
+            ;;
+    esac
+done
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application composer install --no-dev" 
 docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:schema:create --no-interaction
-docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:fixtures:load --no-interaction
-docker exec sem-keyword_php-apache bash -c "cd /app && gosu application yarn encore dev"
+if  ["$fixtures"=1]; then
+    docker exec sem-keyword_php-apache gosu application php /app/bin/console doctrine:fixtures:load --no-interaction
+fi
+docker exec sem-keyword_php-apache bash -c "cd /app && gosu application yarn encore prod"
